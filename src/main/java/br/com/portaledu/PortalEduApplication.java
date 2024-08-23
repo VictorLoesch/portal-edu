ca13cbffc609563f8f43c7ba3c52bfa452286a1e
VictorLoesch <victorsuto10@hotmail.com>
2024-08-22 21:47:40 -0300

    Adicionando projeto ao repositório

---
+++ src/main/java/br/com/portaledu/PortalEduApplication.java
@@ -0,0 +1,13 @@
+package br.com.portaledu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalEduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalEduApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/portaledu/PortalEduApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.portaledu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalEduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
