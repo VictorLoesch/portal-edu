324cef49e968cdb939a5549efbbc38e9b149c287
VictorLoesch <victorsuto10@hotmail.com>
2024-08-27 22:47:42 -0300

    Criação da entidade Aluno

---
+++ src/main/java/br/com/portaledu/model/Aluno.java
@@ -0,0 +1,18 @@
+package br.com.portaledu.model;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class Aluno extends Usuario {
+	
+	
+
+}
