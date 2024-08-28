32ffb36f1a4836254161553eaf4ce9fbbd2bc251
VictorLoesch <victorsuto10@hotmail.com>
2024-08-27 22:47:54 -0300

    Criação da entidade Curso

---
+++ src/main/java/br/com/portaledu/model/Conteudo.java
@@ -0,0 +1,31 @@
+package br.com.portaledu.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Data
+@SuperBuilder
+@NoArgsConstructor
+public class Conteudo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private TipoConteudo tipo;
+	
+	@Column(length = 1000, nullable = false)
+	private String url;
+	
+}
