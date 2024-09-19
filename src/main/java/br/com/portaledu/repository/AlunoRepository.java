828846a0cdf5fa5222a1e1b37bf1295dee8b4de5
VictorLoesch <victorsuto10@hotmail.com>
2024-09-19 19:45:55 -0300

    Adição de metodos no repositorios

--- src/main/java/br/com/portaledu/repository/AlunoRepository.java
+++ src/main/java/br/com/portaledu/repository/AlunoRepository.java
@@ -1,11 +1,23 @@
 package br.com.portaledu.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.portaledu.model.Aluno;
 
 @Repository
 public interface AlunoRepository extends JpaRepository<Aluno, Long> {
 
+	Aluno findByEmail(String email);
+	
+	
+	
+	@Modifying
+    @Transactional
+    @Query("DELETE FROM Aluno a WHERE a.email = ?1")
+    void deleteByEmail(String email);
+	
 }
